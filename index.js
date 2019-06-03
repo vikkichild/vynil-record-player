320301fdb4d5e09740acc30f425946cfa7dfe99e
vikkichild <vikkichild137@gmail.com>
2019-06-03 11:55:38 +0300

    add vinyl rotation

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var button = document.getElementsByClassName('button')[0];
 var vinylRecord = document.getElementsByClassName('vinyl_record')[0];
 
-button.onclick = function rotation() {
-    vinylRecord.classList.toggle('move');
+function rotation() {
+    vinylRecord.classList.toggle("vinyl_record_animation");
 }
+
+button.addEventListener("click", rotation);
