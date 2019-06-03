df884211b356dd68c8e656c27963cc058974ecb1
vikkichild <vikkichild137@gmail.com>
2019-06-03 16:04:53 +0300

    add music player

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 var button = document.getElementsByClassName('button')[0];
 var vinylRecord = document.getElementsByClassName('vinyl_record')[0];
 
-function rotation() {
-    vinylRecord.classList.toggle("vinyl_record_animation");
+function rotation() {    
+    if( vinylRecord.classList.length === 1){
+        vinylRecord.classList.add("vinyl_record_animation");
+        track.play();
+    }
+    else {
+        vinylRecord.classList.remove("vinyl_record_animation");
+        track.pause();
+    }
 }
 
 button.addEventListener("click", rotation);
+
+var track = new Howl({
+    src: ["Wake Owl -  Gold.mp3"],
+    autoplay: false,
+    loop: false,
+    onend: function(){
+        vinylRecord.classList.remove("vinyl_record_animation");
+    }
+});
