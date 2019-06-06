3c559010ba040924d116621a702b76484f52f253
vikkichild <vikkichild137@gmail.com>
2019-06-06 12:09:08 +0300

    add song selection

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+"use strict"
 var button = document.getElementsByClassName('button')[0];
 var vinylRecord = document.getElementsByClassName('vinyl_record')[0];
+var selector = document.getElementById('selector');
 
 function rotation() {    
     if( vinylRecord.classList.length === 1){
         vinylRecord.classList.add("vinyl_record_animation");
+        track = new Howl({
+            src: [musicGenre(selector.value)]
+        });
         track.play();
     }
     else {
@@ -15,10 +20,19 @@ function rotation() {
 button.addEventListener("click", rotation);
 
 var track = new Howl({
-    src: ["Wake Owl -  Gold.mp3"],
+    src: [musicGenre(selector.value)],
     autoplay: false,
     loop: false,
     onend: function(){
         vinylRecord.classList.remove("vinyl_record_animation");
     }
 });
+
+function musicGenre(type) {
+    var songs = {
+        "chill": "chill.mp3",
+        "party": "party.mp3",
+        "relax": "relax.mp3"
+    }
+    return songs[type];
+}
